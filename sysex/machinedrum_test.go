c3908e3d55a8028e7a26578a957ef91962869a68
Ronald Moetwil <ronald.moetwil@gmail.com>
2020-10-31 10:09:14 +0100

    Error formatting not needed

--- sysex/machinedrum_test.go
+++ sysex/machinedrum_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestValidateMachineDrumSysEx_valid(t *testing.T) {
 	if !ValidateMachineDrumSysEx([]byte{0xF0, 0x00, 0x20, 0x3c, 0x02, 0x00}) {
-		t.Errorf("Not a machine drum id")
+		t.Error("Not a machine drum id")
 	}
 }
 
 func TestValidateMachineDrumSysEx_invalid(t *testing.T) {
 	if ValidateMachineDrumSysEx([]byte{0xF0, 0x00, 0x20, 0x3c, 0x03, 0x00}) {
-		t.Errorf("A machine drum id")
+		t.Error("A machine drum id")
 	}
 }
--- sysex/monomachine_test.go
+++ sysex/monomachine_test.go
@@ -4,12 +4,12 @@ import "testing"
 
 func TestValidateMonoMachineSysEx_valid(t *testing.T) {
 	if !ValidateMonoMachineSysEx([]byte{0xF0, 0x00, 0x20, 0x3c, 0x03, 0x00}) {
-		t.Errorf("Not a monomachine id")
+		t.Error("Not a monomachine id")
 	}
 }
 
 func TestValidateMonoMachineSysEx_invalid(t *testing.T) {
 	if ValidateMonoMachineSysEx([]byte{0xF0, 0x00, 0x20, 0x3c, 0x02, 0x00}) {
-		t.Errorf("A monomachine id")
+		t.Error("A monomachine id")
 	}
 }
